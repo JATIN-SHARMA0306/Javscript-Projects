c65d5796e38ad9c5751c520520c4bd586e35bf44
Jatin Sharma <joy.victor1999@gmail.com>
2020-08-18 19:53:46 +0530

    Update WordCount.js

--- WordCounter/WordCount.js
+++ WordCounter/WordCount.js
@@ -1,16 +1,14 @@
 function countWords(){
-let nextLine=0;
+let nextLine=0;//value initialisation 
 let input=document.getElementById("WordsInput").value;
 let output=document.getElementById("output");
-let result=input.split(" ").length;
+let result=input.split(" ").length;// tracking spaces
 try{
-nextLine=input.split(" ").join("").match(/\n/g).length;
+nextLine=input.split(" ").join("").match(/\n/g).length;//tracking nextline
 }
 catch(err){
 nextLine=0;    
-console.log(nextLine);
 }
-console.log(nextLine);
 output.innerHTML=`Total words :- ${result+nextLine}`;
 console.log(result);
 console.log(input.split(" "));
